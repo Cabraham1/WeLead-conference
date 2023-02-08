a738b24128eb17b3239ca4fcc457694555b0ca94
cabraham1 <christopherabraham8@gmail.com>
2023-02-08 15:36:42 +0100

    Add title to aboout page

--- index.js
+++ index.js
@@ -10,14 +10,3 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
-
-// smooth scrooling implmentation
-
-const navLinks = document.querySelectorAll('nav a');
-navLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    const targetSection = document.querySelector(e.target.getAttribute('href'));
-    targetSection.scrollIntoView({ behavior: 'smooth' });
-  });
-});
