daaa15aff42d364c42731fcdfa98d7c661acb0c5
cabraham1 <christopherabraham8@gmail.com>
2023-02-09 08:48:14 +0100

    fix linters errors and refactor speaker js file

--- js/speakerData.js
+++ js/speakerData.js
@@ -63,18 +63,3 @@ speakerBio.innerHTML = speakerArray
     </div>`,
   )
   .join('');
-
-// hamburger
-
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
-
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
-}));
